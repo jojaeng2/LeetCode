e9111718d5a53783a43fe7aebdf830bf8f7472d0
조정현 <76645095+jojaeng2@users.noreply.github.com>
2022-11-28 01:03:17 +0900

    Time: 148 ms (97.22%), Space: 35.5 MB (44.44%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun minimumSum(num: Int): Int {
+        val res = intToList(num).sorted()
+        val li1 : MutableList<Int> = mutableListOf()
+        val li2 : MutableList<Int> = mutableListOf()
+        for(i in 0..res.size-1) {
+            if (i % 2 == 0) li1.add(res[i])
+            else li2.add(res[i])
+        }
+        var answer = 0
+        answer += listSum(li1)
+        answer += listSum(li2)
+        return answer
+    }
+
+    fun listSum(nums : List<Int>) : Int = nums.reduce{sum, num -> sum * 10 + num}
+
+    fun intToList(num : Int) : List<Int> {
+        val li : MutableList<Int> = mutableListOf()
+        var number = num
+        while (number != 0) {
+            li.add(number%10)
+            number /= 10
+        }
+        return li
+    }
+}
