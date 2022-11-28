d0ca7ac082323a0faac21bfad34a626e872ecc3e
조정현 <76645095+jojaeng2@users.noreply.github.com>
2022-11-28 15:06:05 +0900

    Time: 297 ms (94.12%), Space: 40.3 MB (82.35%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.kt
@@ -0,0 +1,41 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+import kotlin.math.max
+
+class Solution {
+    fun deepestLeavesSum(root: TreeNode?): Int {
+        val maxi : Int = getMaxDepth(root)
+        return dfs(root, maxi, 1)
+    }
+    
+    fun dfs(node : TreeNode?, depth : Int, curr : Int) : Int {
+        if(node == null) return 0
+        if (isEqual(depth, curr)) {
+            return getValue(node)
+        }
+        return dfs(node!!.left, depth, curr+1) + dfs(node!!.right, depth, curr+1)
+    }
+    
+    fun getMaxDepth(node : TreeNode?) : Int {   
+        if(node == null) return 0
+        return max(
+            getMaxDepth(node!!.left) + 1, 
+            getMaxDepth(node!!.right) + 1
+        )
+    }
+    
+    fun getValue(node : TreeNode) : Int {
+        return node.`val`
+    }
+    
+    fun isEqual(a : Int, b : Int) : Boolean = (a == b)
+}
